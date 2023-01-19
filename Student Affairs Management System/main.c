f4c135c23b652b6934d124488400da150701e208
Manely Hamedani <manely.hamedani@icloud.com>
2023-01-19 23:16:04 +0330

    Initial Commit

---
+++ Student Affairs Management System/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Student Affairs Management System
+//
+//  Created by Manely Hamedani on 1/19/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
